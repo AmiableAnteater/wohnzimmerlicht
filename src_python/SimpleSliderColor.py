cebf03ae558589f4db51aeedc51a2fce84ff26dd
unknown <ZielkeAndreas@FM-TB2549.stmf.bayern.de>
2017-03-08 07:51:23 +0100

    .

--- src_python/SimpleSliderColor.py
+++ src_python/SimpleSliderColor.py
@@ -1,19 +1,10 @@
+from WS2801 import WS2801Wrapper
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 import sys
-import RPi.GPIO as GPIO
-
-# Import the WS2801 module.
-import Adafruit_WS2801
-import Adafruit_GPIO.SPI as SPI
 
 
-# Configure the count of pixels:
-PIXEL_COUNT = 93
-
-# Alternatively specify a hardware SPI connection on /dev/spidev0.0:
-SPI_PORT   = 0
-SPI_DEVICE = 0
-pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
+pixels = WS2801Wrapper()
+PIXEL_COUNT = pixels.count()
 
 clients = []
 class SimpleSliderColor(WebSocket):
@@ -28,7 +19,7 @@ class SimpleSliderColor(WebSocket):
                      client.sendMessage('X') 
             return
 
-       	base_g, base_b, base_r = pixels.get_pixel_rgb(0)
+       	base_r, base_g, base_b = pixels.get_pixel_rgb(0)
         #print('got values ', base_r, base_g, base_b)
         val = int(self.data[1:])
         if col == "r":
@@ -39,7 +30,7 @@ class SimpleSliderColor(WebSocket):
             base_b = val
         #print('changing to ', base_r, base_g, base_b)
         for n in range(PIXEL_COUNT):
-            pixels.set_pixel_rgb(n, base_g, base_b, base_r)
+            pixels.set_pixel_rgb(n, base_r, base_g, base_b)
         pixels.show()
         for client in clients:
             if client != self:
@@ -48,7 +39,7 @@ class SimpleSliderColor(WebSocket):
     def handleConnected(self):
         #print (self.address, 'connected')
         clients.append(self)
-        g, b, r = pixels.get_pixel_rgb(0)
+        r, g, b = pixels.get_pixel_rgb(0)
         self.sendMessage('r' + str(r))
         self.sendMessage('g' + str(g))
         self.sendMessage('b' + str(b))
---
+++ src_python/WS2801/WS2801Wrapper.py
@@ -0,0 +1,62 @@
+import sys
+import RPi.GPIO as GPIO
+
+# Import the WS2801 module.
+import Adafruit_WS2801
+import Adafruit_GPIO.SPI as SPI
+
+PIXEL_COUNT = 93
+
+
+class WS2801Wrapper (Adafruit_WS2801.WS2801Pixels):
+    def __init__(self, count, clk=None, do=None, spi=None, gpio=None):
+        Adafruit_WS2801.WS2801Pixels.__init__(self, count, clk, do, spi, gpio)
+
+    def __init__(self):
+        Adafruit_WS2801.WS2801Pixels.__init__(self, PIXEL_COUNT, spi=SPI.SpiDev(0, 0), gpio=GPIO)
+
+    def set_pixel_rgb(self, n, r, g, b):
+        """Set the specified pixel n to the provided 8-bit red, green, blue
+        component values.  Note you MUST call show() after setting pixels to
+        see the LEDs change color!
+        """
+        assert 0 <= n < self._count, 'Pixel n outside the count of pixels!'
+        self._pixels[n*3] = g & 0xFF
+        self._pixels[n*3+1] = b & 0xFF
+        self._pixels[n*3+2] = r & 0xFF
+
+    def get_pixel_rgb(self, n):
+        """Retrieve the 8-bit red, green, blue component color values of the
+        specified pixel n.  Will return a 3-tuple of red, green, blue data.
+        """
+        assert 0 <= n < self._count, 'Pixel ' + str(n) + ' outside the count of pixels (' + str(self._count) + ')!'
+        return self._pixels[n*3+2], self._pixels[n*3], self._pixels[n*3+1]
+
+    def add_rgb(self, n, r, g, b):
+        base_b, base_g, base_r = self.get_pixel_rgb(n)
+        self.set_pixel_rgb(n, min(255, r + base_r), min(255, g + base_g), min(255, b + base_b))
+
+    def add_fractional_rgb(self, left_pixel, right_fraction, r, g, b):
+        right_r = round(right_fraction * r)
+        right_g = round(right_fraction * g)
+        right_b = round(right_fraction * b)
+        left_r = r - right_r
+        left_g = g - right_g
+        left_b = b - right_b
+        self.add_rgb(left_pixel, left_r, left_g, left_b)
+        self.add_rgb(left_pixel + 1, right_r, right_g, right_b)
+
+
+if __name__ == "__main__":
+    pixels = WS2801Wrapper()
+    # Clear all the pixels to turn them off.
+    pixels.clear()
+    pixels.show()  # Make sure to call show() after changing any pixels!
+
+    for i in range(pixels.count()):
+        r = int(sys.argv[1]) & 0xFF
+        g = int(sys.argv[2]) & 0xFF
+        b = int(sys.argv[3]) & 0xFF
+        pixels.set_pixel_rgb(i, r, g, b)
+    pixels.show()
+
